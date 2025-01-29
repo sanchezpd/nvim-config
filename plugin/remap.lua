a8ae060cf4c656c230ebb9097150b74f3c286a56
sanchezpd <163014723+sanchezpd@users.noreply.github.com>
2025-01-29 09:22:07 -0600

    updated rename symbol shortcut

--- plugin/remap.lua
+++ plugin/remap.lua
@@ -23,4 +23,5 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
 
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>s", ":lua vim.lsp.buf.rename(<C-r><C-w>)<CR>")
+-- vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
