4f24964f15bb5202c554d7ca07cce57a7053f0c0
sanchezpd <163014723+sanchezpd@users.noreply.github.com>
2025-01-29 10:23:14 -0600

    removed angularlsp. TODO: configure angularlsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,7 +108,7 @@ return {
 
             require('mason-lspconfig').setup({
                 ensure_installed = {
-                    'angularls',
+                    -- 'angularls',
                     'clangd',
                     'csharp_ls',
                     'cssls',
