521313e2f3faaa2d344dbbb75c242e212f094306
sanchezfp <daniel.sanchez@felixpago.com>
2025-12-22 19:57:31 -0600

    🔥 Disable legacy remote providers

--- lua/config/options/global.lua
+++ lua/config/options/global.lua
@@ -1,5 +1,14 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+for _, provider in ipairs({
+  "node",
+  "perl",
+  "python3",
+  "ruby",
+}) do
+  vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
